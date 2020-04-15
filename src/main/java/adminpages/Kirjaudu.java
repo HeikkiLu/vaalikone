e848fbb2963dec7ceeaf1cae5ccfb45c4f8fb8d2
Nestori Metsäranta <nestorimetsaranta@outlook.com>
2020-04-15 14:49:51 +0300

    Kirjaudu.javan lisääminen

---
+++ src/main/java/adminpages/Kirjaudu.java
@@ -0,0 +1,74 @@
+package adminpages;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "Kirjaudu",
+    urlPatterns = {"/kirjaudu"}
+)
+public class Kirjaudu extends HttpServlet {
+
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+@Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException, ServletException {
+	response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+
+    String un = request.getParameter("username");
+    String upw = request.getParameter("password");
+    String pw = "admin";
+    String uun = "admin";
+    String passmd5 = crypt(pw);
+    String upassmd5 = crypt(upw);
+    response.setContentType("text/html;charset=UTF-8");
+    try {
+
+    	if(passmd5.contentEquals(upassmd5) && un.contentEquals(uun)) {
+    		response.getWriter().println("Tervetuloa: " + uun);
+    		//TODO: ohjaa admincontrolpaneeliin
+    	} else {
+    		response.getWriter().println("V‰‰r‰ salasana tai k‰ytt‰j‰nimi");
+    	}
+    } catch(Exception e) {
+    
+    }
+
+  }
+	public static String crypt(String str) {
+        if (str == null || str.length() == 0) {
+            throw new IllegalArgumentException("String to encript cannot be null or zero length");
+        }
+
+        MessageDigest digester;
+        try {
+            digester = MessageDigest.getInstance("MD5");
+
+            digester.update(str.getBytes());
+            byte[] hash = digester.digest();
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < hash.length; i++) {
+                if ((0xff & hash[i]) < 0x10) {
+                    hexString.append("0" + Integer.toHexString((0xFF & hash[i])));
+                } else {
+                    hexString.append(Integer.toHexString(0xFF & hash[i]));
+                }
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
